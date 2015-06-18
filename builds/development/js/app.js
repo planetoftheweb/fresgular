1182f83f774db2d1652627d6ea72043c8afbd40f
Ray Villalobos <ray@planetoftheweb.com>
2015-06-17 23:31:57 -0700

    separated into partials

---
+++ builds/development/js/app.js
@@ -0,0 +1,17 @@
+"use strict";
+ 
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'artistControllers'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/list', {
+    templateUrl: 'partials/list.html',
+    controller: 'ListController'
+  }).
+  otherwise({
+    redirectTo: '/list'
+  });
+}]);
--- builds/development/js/controllers.js
+++ builds/development/js/controllers.js
@@ -1,11 +1,10 @@
 "use strict";
 
-var myApp = angular.module('myApp', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
   });
 }]);
-
