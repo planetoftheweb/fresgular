08039be5233f800cc46e0d849ec6c679f8e966ba
Ray Villalobos <ray@planetoftheweb.com>
2015-06-18 05:39:33 -0700

    added constant

--- builds/development/js/controllers.js
+++ builds/development/js/controllers.js
@@ -3,17 +3,17 @@
 var artistControllers = angular.module('artistControllers', []);
 
 artistControllers.controller('ListController', 
-	['$scope', '$firebaseArray',
-	function($scope, $firebaseArray) {
-		var ref = new Firebase('https://artists.firebaseio.com');
+	['$scope', '$firebaseArray', 'FIREBASE_URL', 
+	function($scope, $firebaseArray, FIREBASE_URL) {
+		var ref = new Firebase(FIREBASE_URL);
 		$scope.artists = $firebaseArray(ref);
     $scope.artistOrder = 'name';
 }]);
 
 artistControllers.controller('DetailsController', 
-	['$scope', '$firebaseArray','$routeParams',
-	function($scope, $firebaseArray, $routeParams) {
-		var ref = new Firebase('https://artists.firebaseio.com');
+	['$scope', '$firebaseArray','$routeParams', 'FIREBASE_URL',
+	function($scope, $firebaseArray, $routeParams, FIREBASE_URL) {
+		var ref = new Firebase(FIREBASE_URL);
 		$scope.artists = $firebaseArray(ref);
     $scope.whichItem = $routeParams.itemId;
 }]);
