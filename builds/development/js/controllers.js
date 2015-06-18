155aa00781ff9cb33b3c446e218164c7a2a0d9a3
Ray Villalobos <ray@planetoftheweb.com>
2015-06-17 22:22:40 -0700

    first commit

---
+++ builds/development/js/controllers.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });
+}]);
+
